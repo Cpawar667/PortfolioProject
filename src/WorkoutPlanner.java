212581466a10a3268ac215098f7e923acd8839b8
Cpawar667 <pawar.71@buckeyemail.osu.edu>
2025-10-22 14:27:02 -0400

    interface

--- src/WorkoutPlanner.java
+++ src/WorkoutPlanner.java
@@ -1,3 +1,60 @@
-public interface workoutinterface {
+import components.map.Map;
+import components.sequence.Sequence;
 
+/**
+ * WorkoutPlanner enhanced interface. * This interface extends the
+ * WorkoutPlannerKernel by adding secondary methods necessary for practical use,
+ * such as updating soreness levels and generating recommended training and rest
+ * plans. * @author Christian Pawar
+ * 
+ * @param <M>
+ *            type of the domain (MuscleGroup) entries
+ * @param <S>
+ *            type of the range (SorenessLevel) entries
+ */
+public interface WorkoutPlanner<M, S> extends WorkoutPlannerKernel<M, S> {
+
+    /**
+     * Replaces the soreness level associated with the given muscle group key
+     * with {@code newSorenessLevel} and returns the old soreness level.
+     * * @param muscleGroup the muscle group (key) whose value is to be replaced
+     * 
+     * @param newSorenessLevel
+     *            the new soreness level (value)
+     * @return the old soreness level
+     * @updates this
+     * @requires muscleGroup is in DOMAIN(this)
+     * @ensures <pre>
+     * this = (#this \ {(muscleGroup, oldSorenessLevel)}) union {(muscleGroup, newSorenessLevel)}  and
+     * oldSorenessLevel = #this.value(muscleGroup)
+     * </pre>
+     */
+    S replaceValue(M muscleGroup, S newSorenessLevel);
+
+    /**
+     * Generates a workout plan consisting of a Sequence of muscle groups that
+     * are suitable for training (where soreness is less than the specified
+     * mandatory rest level). * @param mandatoryRestLevel the SorenessLevel that
+     * indicates a mandatory rest/avoid status
+     * 
+     * @return a Sequence of muscle groups recommended for today's training
+     * @ensures <pre>
+     * generateTrainingPlan = muscleGroups that satisfy the training condition
+     * this = #this
+     * </pre>
+     */
+    Sequence<M> generateTrainingPlan(S mandatoryRestLevel);
+
+    /**
+     * Generates a Map of muscle groups that are too sore to train, along with
+     * their recommended rest messages. * @param mandatoryRestLevel the
+     * SorenessLevel that indicates a mandatory rest/avoid status
+     * 
+     * @return a Map containing muscle groups that require rest
+     * @ensures <pre>
+     * generateRestMap = muscleGroups that satisfy the rest condition
+     * this = #this
+     * </pre>
+     */
+    Map<M, S> generateRestMap(S mandatoryRestLevel);
 }
--- src/WorkoutPlannerKernel.java
+++ src/WorkoutPlannerKernel.java
@@ -1,3 +1,103 @@
-public interface kernelinterface {
+import components.standard.Standard;
 
+/**
+ * WorkoutPlanner kernel interface. * @author Christian Pawar
+ * 
+ * @param <M>
+ *            type of the domain (MuscleGroup) entries
+ * @param <S>
+ *            type of the range (SorenessLevel) entries * @mathmodel <pre>
+ * type WorkoutPlanner is (
+ * (M, S) is a partial function
+ * )
+ * </pre>
+ */
+public interface WorkoutPlannerKernel<M, S>
+        extends Standard<WorkoutPlanner<M, S>> {
+
+    /**
+     * Nested interface for a single key-value pair in the WorkoutPlanner.
+     */
+    public interface Pair<M, S> {
+        /**
+         * Reports the key of the pair. * @return the key
+         * 
+         * @ensures key = this.key
+         */
+        M key();
+
+        /**
+         * Reports the value of the pair. * @return the value
+         * 
+         * @ensures value = this.value
+         */
+        S value();
+    }
+
+    /**
+     * Adds a new muscle status pair to {@code this}. * @param muscleGroup the
+     * muscle group (key) to be added
+     * 
+     * @param sorenessLevel
+     *            the soreness level (value) associated with the key
+     * @updates this
+     * @requires muscleGroup is not in DOMAIN(this)
+     * @ensures this = #this union {(muscleGroup, sorenessLevel)}
+     */
+    void add(M muscleGroup, S sorenessLevel);
+
+    /**
+     * Removes a muscle status pair with the given key from {@code this} and
+     * returns it. * @param muscleGroup the muscle group (key) to be removed
+     * 
+     * @return the removed pair
+     * @updates this
+     * @requires muscleGroup is in DOMAIN(this)
+     * @ensures <pre>
+     * this = #this \ {remove.key, remove.value}  and
+     * remove.key = muscleGroup  and
+     * remove.value = #this.value(muscleGroup)
+     * </pre>
+     */
+    Pair<M, S> remove(M muscleGroup);
+
+    /**
+     * Removes an arbitrary muscle status pair from {@code this} and returns it.
+     * * @return the removed pair
+     * 
+     * @updates this
+     * @requires |this| > 0
+     * @ensures <pre>
+     * this = #this \ {remove.key, remove.value}  and
+     * remove is in #this
+     * </pre>
+     */
+    Pair<M, S> removeAny();
+
+    /**
+     * Reports the soreness level associated with the given muscle group key in
+     * {@code this}. * @param muscleGroup the muscle group (key) to query
+     * 
+     * @return the soreness level associated with the muscle group
+     * @requires muscleGroup is in DOMAIN(this)
+     * @ensures value = this.value(muscleGroup)
+     */
+    S value(M muscleGroup);
+
+    /**
+     * Reports whether or not {@code muscleGroup} is in the domain of
+     * {@code this}. * @param muscleGroup the muscle group (key) to check
+     * 
+     * @return true if muscleGroup is in DOMAIN(this), false otherwise
+     * @ensures hasKey = (muscleGroup is in DOMAIN(this))
+     */
+    boolean hasKey(M muscleGroup);
+
+    /**
+     * Reports the number of muscle status pairs in {@code this}. * @return the
+     * size of {@code this}
+     * 
+     * @ensures size = |this|
+     */
+    int size();
 }
