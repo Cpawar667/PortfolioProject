9464797f24cd77eeae487c9eec4480526b7ed099
Cpawar667 <pawar.71@buckeyemail.osu.edu>
2025-11-06 17:19:28 -0500

    interfaces changed
    Uses map under hood

--- src/WorkoutPlannerKernel.java
+++ src/WorkoutPlannerKernel.java
@@ -1,102 +1,115 @@
+import components.map.Map;
 import components.standard.Standard;
 
 /**
- * WorkoutPlanner kernel interface. * @author Christian Pawar
- * 
- * @param <M>
- *            type of the domain (MuscleGroup) entries
- * @param <S>
- *            type of the range (SorenessLevel) entries * @mathmodel <pre>
- * type WorkoutPlanner is (
- * (M, S) is a partial function
- * )
+ * WorkoutPlanner kernel interface.
+ *
+ * @author Christian Pawar
+ *
+ * @mathmodel <pre>
+ * type WorkoutPlanner is modeled as a partial function from MuscleGroup to SorenessLevel
+ * </pre>
+ * @initially <pre>
+ * default:
+ *  ensures
+ *   this = {}
+ * </pre>
+ * @iterator <pre>
+ * ~this.seen * ~this.unseen = this  and
+ * |~this.seen * ~this.unseen| = |this|
  * </pre>
  */
-public interface WorkoutPlannerKernel<M, S>
-        extends Standard<WorkoutPlanner<M, S>> {
+public interface WorkoutPlannerKernel extends Standard<WorkoutPlanner> {
 
     /**
-     * Nested interface for a single key-value pair in the WorkoutPlanner.
-     */
-    public interface Pair<M, S> {
-        /**
-         * Reports the key of the pair. * @return the key
-         * 
-         * @ensures key = this.key
-         */
-        M key();
-
-        /**
-         * Reports the value of the pair. * @return the value
-         * 
-         * @ensures value = this.value
-         */
-        S value();
-    }
-
-    /**
-     * Adds a new muscle status pair to {@code this}. * @param muscleGroup the
-     * muscle group (key) to be added
-     * 
+     * Adds a new muscle status pair to {@code this}.
+     *
+     * @param muscleGroup
+     *            the muscle group (key) to be added
      * @param sorenessLevel
      *            the soreness level (value) associated with the key
      * @updates this
      * @requires muscleGroup is not in DOMAIN(this)
      * @ensures this = #this union {(muscleGroup, sorenessLevel)}
      */
-    void add(M muscleGroup, S sorenessLevel);
+    void add(MuscleGroup muscleGroup, SorenessLevel sorenessLevel);
 
     /**
-     * Removes a muscle status pair with the given key from {@code this} and
-     * returns it. * @param muscleGroup the muscle group (key) to be removed
-     * 
-     * @return the removed pair
+     * Removes the muscle status pair with the given key from {@code this} and
+     * returns the associated soreness level.
+     *
+     * @param muscleGroup
+     *            the muscle group (key) to be removed
+     * @return the soreness level that was associated with muscleGroup
      * @updates this
      * @requires muscleGroup is in DOMAIN(this)
      * @ensures <pre>
-     * this = #this \ {remove.key, remove.value}  and
-     * remove.key = muscleGroup  and
-     * remove.value = #this.value(muscleGroup)
+     * remove = #this.value(muscleGroup) and
+     * this = #this \ {(muscleGroup, remove)}
      * </pre>
      */
-    Pair<M, S> remove(M muscleGroup);
+    SorenessLevel remove(MuscleGroup muscleGroup);
 
     /**
      * Removes an arbitrary muscle status pair from {@code this} and returns it.
-     * * @return the removed pair
-     * 
+     *
+     * @return the removed pair (using Map.Pair from OSU Components)
      * @updates this
      * @requires |this| > 0
      * @ensures <pre>
-     * this = #this \ {remove.key, remove.value}  and
-     * remove is in #this
+     * removeAny is in #this and
+     * this = #this \ {removeAny}
      * </pre>
      */
-    Pair<M, S> removeAny();
+    Map.Pair<MuscleGroup, SorenessLevel> removeAny();
 
     /**
      * Reports the soreness level associated with the given muscle group key in
-     * {@code this}. * @param muscleGroup the muscle group (key) to query
-     * 
+     * {@code this}.
+     *
+     * @param muscleGroup
+     *            the muscle group (key) to query
      * @return the soreness level associated with the muscle group
      * @requires muscleGroup is in DOMAIN(this)
      * @ensures value = this.value(muscleGroup)
      */
-    S value(M muscleGroup);
+    SorenessLevel value(MuscleGroup muscleGroup);
 
     /**
      * Reports whether or not {@code muscleGroup} is in the domain of
-     * {@code this}. * @param muscleGroup the muscle group (key) to check
-     * 
+     * {@code this}.
+     *
+     * @param muscleGroup
+     *            the muscle group (key) to check
      * @return true if muscleGroup is in DOMAIN(this), false otherwise
      * @ensures hasKey = (muscleGroup is in DOMAIN(this))
      */
-    boolean hasKey(M muscleGroup);
+    boolean hasKey(MuscleGroup muscleGroup);
 
     /**
-     * Reports the number of muscle status pairs in {@code this}. * @return the
-     * size of {@code this}
-     * 
+     * MuscleGroup enumeration used to identify muscle groups.
+     */
+    public static enum MuscleGroup {
+        CHEST, BACK, LEGS, ARMS, SHOULDERS, CORE
+    }
+
+    /**
+     * SorenessLevel enumeration used to indicate soreness severity.
+     */
+    public static enum SorenessLevel {
+        NONE(0), LOW(1), MEDIUM(2), HIGH(3);
+
+        private final int level;
+
+        SorenessLevel(int level) {
+            this.level = level;
+        }
+    }
+
+    /**
+     * Reports the number of muscle status pairs in {@code this}.
+     *
+     * @return the size of {@code this}
      * @ensures size = |this|
      */
     int size();
